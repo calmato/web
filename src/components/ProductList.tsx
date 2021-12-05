e354d19ca6282c4025b5dd0caa130b274547fd80
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2021-12-05 12:31:32 +0900

    fix: adjust title style

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -22,7 +22,7 @@ export function ProductList(props: Props) {
   return (
     <Grid templateColumns="repeat(2, minmax(50%, 1fr))" gap={0}>
       {productList.map((product, idx) => (
-        <ProductItem handleClick={handleItemClick} product={product} key={idx} boxProps={{ mb: 4 }} />
+        <ProductItem handleClick={handleItemClick} product={product} key={idx} />
       ))}
     </Grid>
   );
