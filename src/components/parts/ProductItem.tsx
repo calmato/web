3148603bc40c856af5788a570180f2cebc6554fa
nishi <tadashi-112@hotmail.co.jp>
2021-12-05 13:35:55 +0900

    fix: add padding (#22)
    * fix: add padding
    
    * fix: typo
    
    * fix: adjust title style
    
    * fix: adjust title style
    
    * fix: adjust title style
    
    * fix: adjust title style
    
    * fix: adjust title style
    
    * fix: adjust title style
    
    * fix: adjust title style
    
    * fix: adjust title style
    
    * fix: adjust title style
    
    * fix: adjust title style
    
    * fix: adjust title style
    
    * fix: adjust title style
    
    * fix: adjust title style
    
    * fix: add flex style

--- src/components/parts/ProductItem.tsx
+++ src/components/parts/ProductItem.tsx
@@ -1,4 +1,4 @@
-import { Box, BoxProps, GridItem } from "@chakra-ui/layout";
+import { Box, BoxProps, Flex, GridItem } from "@chakra-ui/layout";
 import { Text } from "@chakra-ui/react";
 import React from "react";
 import { Product } from "../../types";
@@ -13,22 +13,24 @@ export function ProductItem(props: Props) {
   const { product, handleClick } = props;
 
   return (
-    <GridItem as="button" onClick={() => handleClick(product.linkName)} {...props.boxProps}>
-      <Box
-        textAlign="center"
-        borderRadius="xl"
-        backgroundImage={product.imgSrc}
-        backgroundSize="contain"
-        backgroundRepeat="no-repeat"
-        backgroundPosition="center"
-        backgroundColor="gray.50"
-        boxSize="80%"
-        paddingBottom="80%"
-        marginLeft="10%"
-      />
-      <u>
-        <Text textAlign="center">{product.name}</Text>
-      </u>
+    <GridItem as="button" py="8px" onClick={() => handleClick(product.linkName)} {...props.boxProps}>
+      <Flex flexDirection="column">
+        <Box
+          textAlign="center"
+          borderRadius="xl"
+          backgroundImage={product.imgSrc}
+          backgroundSize="contain"
+          backgroundRepeat="no-repeat"
+          backgroundPosition="center"
+          backgroundColor="gray.50"
+          boxSize="80%"
+          paddingBottom="80%"
+          mx="10%"
+        />
+        <Text textAlign="center" textDecoration="underline" height="auto" my="8px">
+          {product.name}
+        </Text>
+      </Flex>
     </GridItem>
   );
 }
