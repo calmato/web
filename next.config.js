74a536032a0612725e36eb4424a653a5cc0325f5
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2021-12-12 00:22:44 +0900

    feat: 一応環境変数でデプロイ先のURLを指定できるようにする

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ module.exports = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_CONTACT_API: process.env.NEXT_PUBLIC_CONTACT_API,
+    NEXT_PUBLIC_HOST_URL: process.env.NEXT_PUBLIC_HOST_URL,
   },
 }
